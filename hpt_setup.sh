16b601535f4703b56bcb4b6e274c81aed562aa27
manjunathan-msd <manjunathan@madstreetden.com>
2024-07-30 13:46:12 +0000

    Changed Bash Once again

--- hpt_setup.sh
+++ hpt_setup.sh
@@ -18,6 +18,12 @@ cd HyperGAI || exit
 # Install Git LFS
 git lfs install
 
+# Create a directory for HPT1_5-Air-Llama-3-8B-Instruct-multimodal
+mkdir -p "HPT1_5-Air-Llama-3-8B-Instruct-multimodal"
+
+# Change directory to the new folder
+cd "HPT1_5-Air-Llama-3-8B-Instruct-multimodal" || exit
+
 # Clone the HPT1_5-Edge repository
 git clone https://huggingface.co/HyperGAI/HPT1_5-Air-Llama-3-8B-Instruct-multimodal .
 
