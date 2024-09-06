b35ef769d5b24fbee7786a90ef0b8b8008321c51
ankur@madstreetden.com <ankur@madstreetden.com>
2024-09-06 12:31:15 +0000

    Unnecessary files removed

--- temp.py
+++
@@ -1,7 +0,0 @@
-# Import libraries
-import pandas as pd
-from fill_taxonomy.utils import FillMetadata
-
-# Read taxonomy
-df = pd.read_csv('/home/ubuntu/efs/users/ankur/tag-product2dotx/dumps/fairprice_beer_hit_remaining_iamge_count_1.csv')
-print(df.shape)
